15bd576c9d4fc9e1bb88346b283b3019e9d0984d
Kevin Winarko <saberkevin27@gmail.com>
2019-01-07 23:05:13 +0700

    dropdown

--- app/src/main/java/binuslabproject/bluelaundry/HomeActivity.java
+++ app/src/main/java/binuslabproject/bluelaundry/HomeActivity.java
@@ -5,6 +5,10 @@ import android.content.SharedPreferences;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -21,6 +25,35 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //spinner menu dropdown
+        Spinner menuDropDown = findViewById(R.id.menuDropDown);
+        ArrayAdapter<String> dropDownAdapter = new ArrayAdapter<String>(HomeActivity.this,
+                R.layout.drop_down_spinner_item, getResources().getStringArray(R.array.menuDropDown));
+        dropDownAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        menuDropDown.setAdapter(dropDownAdapter);
+        menuDropDown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch(position)
+                {
+                    case 0:
+
+                        break;
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         //greetings
         SharedPreferences sp = getSharedPreferences("LoginGlobalVariable", Activity.MODE_PRIVATE);
