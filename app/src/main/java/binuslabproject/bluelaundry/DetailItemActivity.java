c218aeb27f654189431e121e9b34e61f433162bb
Kevin Winarko <saberkevin27@gmail.com>
2019-01-07 09:27:48 +0700

    Detail Item .java update

--- app/src/main/java/binuslabproject/bluelaundry/DetailItemActivity.java
+++ app/src/main/java/binuslabproject/bluelaundry/DetailItemActivity.java
@@ -2,6 +2,9 @@ package binuslabproject.bluelaundry;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class DetailItemActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class DetailItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_item);
+
+        Button btnSubmit = (Button)findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+        btnSubmit.setOnHoverListener(new View.OnHoverListener()
+        {
+            public boolean onHover(View v, MotionEvent event)
+            {
+                return false;
+            }
+        });
     }
+
 }
