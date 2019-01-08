43eab38cd4cb721e4a5c7d89292869354d38d3d0
Kevin Winarko <saberkevin27@gmail.com>
2019-01-09 00:06:43 +0700

    Menu Bar tidy up hello username

--- app/src/main/java/binuslabproject/bluelaundry/HomeActivity.java
+++ app/src/main/java/binuslabproject/bluelaundry/HomeActivity.java
@@ -70,7 +70,7 @@ public class HomeActivity extends AppCompatActivity {
         SharedPreferences sp = getSharedPreferences(LoginActivity.globalPreferenceName, Activity.MODE_PRIVATE);
         String username = sp.getString("username","Anonymous");
         TextView greetings = findViewById(R.id.greetings);
-        greetings.setText("Hello, " + username);
+        greetings.setText("Hello, \n" + username);
 
 
         //carousel
